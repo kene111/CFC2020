df9dc46728a290995900d2f3607388019976e2f2
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-07-31 10:34:20 +0100

    updates

--- web-platform/CODE/settings.py
+++ web-platform/CODE/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] #'ndia.eu-gb.mybluemix.net'
+ALLOWED_HOSTS = ['ndia.eu-gb.mybluemix.net'] #'ndia.eu-gb.mybluemix.net'
 
 
 # Application definition
@@ -157,7 +157,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tocrear.3@gmail.com'
-EMAIL_HOST_PASSWORD = '3L_9sy_Congr00'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
